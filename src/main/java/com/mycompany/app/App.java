52bc3e430659c24dca256aaab3fc47fd0478c4bd
pavan <pavankumar.y.c@greenwayhealth.com>
2022-08-22 16:25:00 +0530

    added print statement to trigger build

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,8 @@ public class App
 
     public static void main(String[] args) {
         System.out.println(new App().getMessage());
+        System.out.println("Java app is running");
+
     }
 
     private final String getMessage() {
